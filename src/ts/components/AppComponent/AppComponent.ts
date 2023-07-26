2a7a437682a9d73aa0d4332708e837c018322999
Raúl Pérez <jrpcassone@gmail.com>
2023-07-27 01:11:08 +0200

    Fix header rendering

--- src/ts/components/AppComponent/AppComponent.ts
+++ src/ts/components/AppComponent/AppComponent.ts
@@ -4,20 +4,21 @@ import TvShowCardComponent from "../TvShowCardComponent/TvShowCardComponent.js";
 class AppComponent extends Component {
   constructor(parentElement: Element) {
     super(parentElement, "div", "container");
-
-    this.tag = this.tag;
   }
 
   render() {
     this.element.innerHTML = `
-    <main class="main-content"></main>
+    <header class="main-header">
+      <h1 class="main-title">My Series</h1>
+      </header>
+        <main class="main-content"></main>
       <h2 class="main-content__title">Series list</h2>
       <section class="list">
         <h3 class="list__title">Pending series</h3>
         <span class="list__info">You have 4 series pending to watch</span>
         <!--<span class="list__info">Congrats! You've watched all your series</span>-->
-        <ul class="series">
-        </ul>`;
+      <ul class="series">
+    </ul>`;
 
     const seriesList = document.querySelector(".series")!;
 
