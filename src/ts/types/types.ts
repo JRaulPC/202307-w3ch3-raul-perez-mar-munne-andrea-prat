90d0d10afd9b2b400e2cecce21dbd77d4200d1a8
Raúl Pérez <jrpcassone@gmail.com>
2023-07-26 19:41:01 +0200

    Add ts types folder

---
+++ src/ts/types/types.ts
@@ -0,0 +1,12 @@
+export interface ShowStructure {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  isWatched: boolean;
+  score: 0 | 1 | 2 | 3 | 4 | 5;
+  emmies: number;
+}
+
+export type Shows = ShowStructure[];
