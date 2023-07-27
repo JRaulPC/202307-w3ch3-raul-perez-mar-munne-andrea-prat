649e06d7c796cd037babc0ec4bc394dffe2927e0
Raúl Pérez <jrpcassone@gmail.com>
2023-07-27 23:17:55 +0200

    Separate list by pending and watched shows

--- src/ts/components/SeriesList/SeriesList.ts
+++ src/ts/components/SeriesList/SeriesList.ts
@@ -4,25 +4,48 @@ import TvShowCardComponent from "../TvShowCardComponent/TvShowCardComponent.js";
 
 class SeriesList extends Component {
   constructor(parentElement: Element) {
-    super(parentElement, "h2", "main-content__title");
+    super(parentElement, "main", "main-content");
   }
 
   render() {
     this.element.innerHTML = ` 
        <section class="list">
         <h3 class="list__title">Pending series</h3>
-        <span class="list__info">You have 4 series pending to watch</span>
-        <!--<span class="list__info">Congrats! You've watched all your series</span>-->
+          <span class="list__info">You have 4 series pending to watch</span>
+          <!--<span class="list__info">Congrats! You've watched all your series</span>-->
         <ul class="series">
-        </section>
-          `;
+         </ul>
+      </section>
+      <section class="list">
+        <h3 class="list__title">Watched series</h3>
+        <span class="list__info">You have watched 4 series</span>
+        <ul class="series">
+         </ul>
+      </section>
+      `;
 
-    const seriesList = document.querySelector(".series")!;
+    const pendingShowsList = this.element.querySelector(
+      ".list:nth-of-type(1) .series"
+    )!;
 
-    shows.slice(0, 4).forEach((tvShow) => {
+    const pendingShows = shows.filter((show) => !show.isWatched);
+
+    pendingShows.forEach((pendingShow) => {
+      const pendingShowPosition = document.createElement("li");
+      new TvShowCardComponent(pendingShowPosition, pendingShow).render();
+      pendingShowsList.append(pendingShowPosition);
+    });
+
+    const watchedShowsList = this.element.querySelector(
+      ".list:nth-of-type(2) .series"
+    )!;
+
+    const watchedShows = shows.filter((show) => show.isWatched);
+
+    watchedShows.forEach((tvShow) => {
       const serielist = document.createElement("li");
       new TvShowCardComponent(serielist, tvShow).render();
-      seriesList.append(serielist);
+      watchedShowsList.append(serielist);
     });
   }
 }
