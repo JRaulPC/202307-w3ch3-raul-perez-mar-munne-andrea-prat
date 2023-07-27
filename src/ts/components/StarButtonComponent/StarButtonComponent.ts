33f7f5fb9b177238249329a4da2385b279bf6496
Mar Munné <mar.munnesteve@gmail.com>
2023-07-27 19:41:32 +0200

    Add StarButtonComponent folder and file WiP

---
+++ src/ts/components/StarButtonComponent/StarButtonComponent.ts
@@ -0,0 +1,32 @@
+import Component from "../Component/Component.js";
+
+class StarButtonComponent extends Component {
+  text: string;
+  actionOnClick: () => void;
+
+  constructor(
+    parentElement: Element,
+    text: string,
+    actionOnClick: () => void,
+    className = "icon icon--score far fa-star"
+  ) {
+    super(parentElement, "score", `${className}`);
+
+    this.text = text;
+    this.actionOnClick = actionOnClick;
+
+    this.listenEvents();
+  }
+
+  render() {
+    this.element.textContent = this.text;
+    this.element.innerHTML = 
+
+  }
+
+  listenEvents() {
+    this.element.addEventListener("click", this.actionOnClick);
+  }
+}
+
+export default StarButtonComponent;
