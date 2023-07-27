44a38313085d814ec12b58bcb35692412baafe0e
Mar Munné <mar.munnesteve@gmail.com>
2023-07-27 21:24:47 +0200

    Start developing star buttons status' methods

--- src/ts/components/StarButtonsComponent/StarButtonsComponent.ts
+++ src/ts/components/StarButtonsComponent/StarButtonsComponent.ts
@@ -2,11 +2,13 @@ import Component from "../Component/Component.js";
 
 class StarButtonsComponent extends Component {
   text: string;
+  isUnfilled: boolean;
   actionOnClick: () => void;
 
   constructor(
     parentElement: Element,
     text: string,
+    isUnfilled: boolean,
     actionOnClick: () => void,
     className = "icon icon--score far fa-star"
   ) {
@@ -14,18 +16,25 @@ class StarButtonsComponent extends Component {
 
     this.text = text;
     this.actionOnClick = actionOnClick;
+    this.isUnfilled = isUnfilled && true;
 
+    this.changeStarButtonsStatus(isUnfilled);
     this.listenEvents();
   }
 
   render() {
     this.element.textContent = this.text;
     this.element.innerHTML = 
+    
 
   }
 
+  changeStarButtonsStatus(isUnfilled: boolean) {
+    this.isUnfilled = !this.isUnfilled;
+  }
+
   listenEvents() {
-    this.element.addEventListener("click", this.actionOnClick);
+    const actionOnClick = this.element.addEventListener("click", this.changeStarButtonsStatus(this.isUnfilled));
   }
 }
 
